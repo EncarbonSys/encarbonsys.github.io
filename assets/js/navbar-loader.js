683ccd290143c292678ceb4ebd8e7b09418a1ce9
EncarbonSys <ashautomates07@gmail.com>
2026-07-16 10:50:33 +0530

    feat: clean case-sensitive EnCBAM URLs and extensionless paths

--- assets/js/navbar-loader.js
+++ assets/js/navbar-loader.js
@@ -255,9 +255,9 @@
             <i class="fas fa-bars"></i>
         </button>
         <ul class="nav-links" id="encav-navLinks">
-            <li><a href="/encbam.html#problem">Problem</a></li>
-            <li><a href="/encbam.html#solution">Solution</a></li>
-            <li><a href="/pages/pricing.html">Pricing</a></li>
+            <li><a href="/EnCBAM#problem">Problem</a></li>
+            <li><a href="/EnCBAM#solution">Solution</a></li>
+            <li><a href="/pages/pricing">Pricing</a></li>
             <li class="dropdown" id="encav-toolsDropdown">
                 <a href="#" class="dropdown-toggle">
                     Tools <i class="fas fa-chevron-down" style="font-size:0.7rem;"></i>
@@ -269,10 +269,10 @@
                 </ul>
             </li>
             <li><a href="/cbam-blog/">Blog</a></li>
-            <li><a href="/encbam.html#faq">FAQ</a></li>
+            <li><a href="/EnCBAM#faq">FAQ</a></li>
             <li class="nav-cta-group" style="display:flex;align-items:center;gap:1rem;margin-left:1rem;">
                 <a href="https://encbam.encarbonsys.com/login" class="nav-login">Login</a>
-                <a href="/encbam.html#cta" class="nav-cta">Get Started</a>
+                <a href="/EnCBAM#cta" class="nav-cta">Get Started</a>
             </li>
         </ul>
     </div>
